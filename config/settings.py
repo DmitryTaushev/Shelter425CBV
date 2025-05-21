70df5d4299288a377f82362a7a1001da8220d418
TaushevDN <123morfix@gmail.com>
2025-05-21 11:48:28 +0500

    1.Добавлена настройка для авторизации пользователя

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ import os
 
 from pathlib import Path
 
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -145,3 +146,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
