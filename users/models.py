15c9e5b0729dfe0fca420da4833ebb8fbde98fd5
TaushevDN <123morfix@gmail.com>
2025-05-28 16:43:55 +0500

    1.Добавлены собственные уровни доступа 2.Добавлено поле role в модель пользователя 3.Созданы и проведены миграции 4.Обновлены фикстуры 5.Расширена команда создания пользователя

--- users/models.py
+++ users/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank':True,'null':True}
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin', _('admin')
+    MODERATOR = 'moderator', _('moderator')
+    USER = 'user', _('user')
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True,verbose_name='Email')
+    role = models.CharField(max_length=9,choices=UserRoles.choices,default=UserRoles.USER)
     first_name = models.CharField(max_length=150,verbose_name='Имя',default='Anonymous')
     phone = models.CharField(unique = True,max_length=35,verbose_name='Телефон',**NULLABLE)
     telegram = models.CharField(max_length=150,unique=True,verbose_name='Телеграмм',**NULLABLE)
