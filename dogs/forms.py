f63fa35d9b46eafedf25858336a9b7926da5d778
TaushevDN <123morfix@gmail.com>
2025-05-30 10:17:40 +0500

    1.Добавлена форма DogAdminForm 2.Добавлен вывод разных форм в зависимости от уровня доступа

--- dogs/forms.py
+++ dogs/forms.py
@@ -17,6 +17,11 @@ class DogForm(StyleFormMixin,forms.ModelForm):
             raise forms.ValidationError('Собака должна быть моложе 35-ти лет')
         return cleaned_data
 
+class DogAdminForm(DogForm):
+    class Meta:
+        model = Dog
+        exclude = ('is_active',)
+
 class DogParentForm(StyleFormMixin,forms.ModelForm):
     class Meta:
         model = DogParent
